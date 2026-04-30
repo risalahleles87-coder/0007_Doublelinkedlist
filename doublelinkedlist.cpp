42995f4780fedd64b8b199b958ea77ba57d31ecc
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-30 19:07:30 +0700

    Menambahkan validasi data duplikat pada addnode

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -65,4 +65,9 @@ public:
      current = current->next;
  }
 
- 
+ if (current->next != NULL && nim == current->next->noMhs)
+ { 
+    cout << "\nDuplicate roll numbers not allowed" << endl;
+    return;
+ }
+
